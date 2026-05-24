3a363a7525aedee0363b79aa7e1da66a56a1513a
K0mii <madison.o.mayer@gmail.com>
2026-05-24 17:44:16 -0500

    i created all my files for the other pages

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// const apple = document.getElementById("apple1");
+const apple = document.getElementById("apple1");
 
-//const images = [
+const images = [
 
-//    "images/apple2.png";
-//    "images/apple3.png";
-//    "images/apple4.png";
+    "images/apple2.png";
+    "images/apple3.png";
+    "images/apple4.png";
 
-//]
+]
 
-//let index = 0;
-//apple.addEventListener("click")
-//    console.log("apple clicks");
-//if (index>= images.length) {
-//    index = 0;
+let index = 0;
+apple.addEventListener("click")
+    console.log("apple clicks");
+if (index>= images.length) {
+    index = 0;
 
-//}
+}

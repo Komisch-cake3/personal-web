d99ce8aed5d05f7c71da55b18b865bb2b9098462
K0mii <madison.o.mayer@gmail.com>
2026-05-24 16:11:12 -0500

    js confuses me so much </3

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const apple = document.getElementById("apple1");
 
+const images = [
+
+    "images/apple2.png";
+    "images/apple3.png";
+    "images/apple4.png";
+
+]
+
+let index = 0;
+apple.addEventListener("click")
+    console.log("apple clicks");
+if (index>= images.length) {
+    index = 0;
+
+}

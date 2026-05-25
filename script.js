b9f87b914204df5e98cf37aa42cdb5fb3fc39417
K0mii <madison.o.mayer@gmail.com>
2026-05-25 12:38:06 -0500

    I redid my original script with ai, the only thing i really did to change it was adjust how the clicks worked with the 0 and the third click etc

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
- const img = document.getElementById('apple1');
 
+const img = document.getElementById('apple1');
+
+let clicks = 0;
  img.addEventListener("click", function(){
-
-    img.src = "images/appl2.png";
-
+    clicks++;
+    if (clicks ===0){
+        img.src = "images/appl1.png"
+    }
+    if (clicks=== 2) {
+            img.src = "images/appl2.png";
+    }
+    if (clicks===3) {
+            img.src = "images/appl3.png";
+           
+    }
+    if (clicks ===4){
+     window.location.href = "home.html"
+    }
  })
+
+ // This part of the code was made with ai. 
